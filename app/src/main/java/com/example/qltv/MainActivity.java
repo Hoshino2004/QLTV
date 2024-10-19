c7f56b1f8dd611345a9ba65d720f2d12d55391e5
Nguyen Thinh <thinh631631@gmail.com>
2024-10-19 18:40:24 +0700

    Cap nhat giao dien v5

--- app/src/main/java/com/example/qltv/MainActivity.java
+++ app/src/main/java/com/example/qltv/MainActivity.java
@@ -4,50 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    CardView cardProfile, cardBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        cardProfile = findViewById(R.id.cardProfile);
+        cardBook = findViewById(R.id.cardBook);
+
+        cardProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PersonalActivity.class));
+            }
+        });
+
+        cardBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BookListActivity.class));
+            }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // Lấy ID của item được chọn
-        int id = item.getItemId();
-
-        // Kiểm tra ID của item
-        switch (id) {
-            case R.id.personal_item:
-                startActivity(new Intent(MainActivity.this, PersonalActivity.class));
-                return true;
-            case R.id.book_list_item:
-                startActivity(new Intent(MainActivity.this, BookListActivity.class));
-                return true;
-            default:
-                // Nếu không có item nào khớp, gọi phương thức của lớp cha
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
